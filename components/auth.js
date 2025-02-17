638fd66a579e43ea4132398b930f59417380396b
Rohit SAROJ <takniqk@gmail.com>
2025-02-17 16:09:14 +0530

    Update auth.js

--- components/auth.js
+++ components/auth.js
@@ -1,1 +1,11 @@
+import jwtDecode from "jwt-decode";
 
+export const setToken = (token) => localStorage.setItem("token", token);
+export const getToken = () => localStorage.getItem("token");
+export const removeToken = () => localStorage.removeItem("token");
+
+export const getUserRole = () => {
+  const token = getToken();
+  if (!token) return null;
+  return jwtDecode(token).role;
+};
