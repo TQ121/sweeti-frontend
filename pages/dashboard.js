a058454c04b3eb9b78c8f311d2097dd6966ec7f6
Rohit SAROJ <takniqk@gmail.com>
2025-02-17 16:06:48 +0530

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,1 +1,19 @@
+import { useEffect, useState } from "react";
+import { getUserRole, removeToken } from "../utils/auth";
+import { useRouter } from "next/router";
 
+export default function Dashboard() {
+  const [role, setRole] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    setRole(getUserRole());
+  }, []);
+
+  return (
+    <div>
+      <h2>Dashboard - {role}</h2>
+      <button onClick={() => { removeToken(); router.push("/login"); }}>Logout</button>
+    </div>
+  );
+}
